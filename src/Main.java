ce8b29e375b7f66487e8b43a9c2cf3d4296cf0a6
alexandrbektashev <aesthat@gmail.com>
2018-03-26 13:17:45 +0300

    final

--- src/Main.java
+++ src/Main.java
@@ -9,58 +9,45 @@ import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-        String input = "roles:\n" +
-                "Городничий\n" +
-                "Аммос Федорович\n" +
-                "Артемий Филиппович\n" +
-                "Лука Лукич\n" +
-                "textLines:\n" +
-                "Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор.\n" +
-                "Аммос Федорович: Как ревизор?\n" +
-                "Артемий Филиппович: Как ревизор?\n" +
-                "Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем.\n" +
-                "Аммос Федорович: Вот те на!\n" +
-                "Артемий Филиппович: Вот не было заботы, так подай!\n" +
-                "Лука Лукич: Господи боже! еще и с секретным предписаньем!";
+        String[] myroles = new String[]{"igor", "ivan", "alex", "masha"};
+        String[] mytext = new String[]{"igor:sentence", "alex:sliva", "alex:persik",
+                "masha:haha how about that much anticipated sport match last night",
+        "ivan: chtoooo???"};
 
+        System.out.println(printTextPerRole(myroles,mytext));
+    }
 
-        String text = String.valueOf(input) + "\n";
-        text = text.substring(text.indexOf('\n') + 1);
-        int rolesCount = 0;
-        String line = "";
-        ArrayList<String> roles = new ArrayList<>();
-        int indexn = 0;
-        String role;
-        while (true){
-            indexn = text.indexOf('\n');
-            line = text.substring(0, indexn);
-            if (line.equals("textLines:")) break;
-            text = text.substring(indexn + 1);
-            System.out.println(line);
-            roles.add(line);
-        }
-        text = text.substring(text.indexOf('\n') + 1);
-        int currrole = 1;
-        ArrayList<String> rolesandtext = new ArrayList<String>(roles);
-        while (!text.equals("")) {
-            indexn = text.indexOf('\n');
-            line = text.substring(0, indexn);
-            text = text.substring(indexn + 1);
-            role = line.substring(0, line.indexOf(':'));
-            line = line.substring(line.indexOf(':') + 1);
-            for (int i = 0; i < roles.size(); i++) {
-                if (roles.get(i).equals(role)) {
-                    rolesandtext.set(i, rolesandtext.get(i) + "\n" + currrole + ")" + line);
-                    currrole ++;
-                    break;
+    private static String printTextPerRole(String[] roles, String[] textLines) {
+
+        int currole = 1;
+        StringBuilder[] rolesandtext = new StringBuilder[roles.length];
+        for (int i = 0; i < roles.length; i++)
+            rolesandtext[i] = new StringBuilder(roles[i] + "\n");
+        StringBuilder text = new StringBuilder();
+        try {
+            for (int i = 0; i < textLines.length; i++) {
+                int indexsep = textLines[i].indexOf(':');
+                String role = textLines[i].substring(0, indexsep);
+                String line = textLines[i].substring(indexsep + 1);
+                for (int j = 0; j < roles.length; j++) {
+                    if (role.equals(roles[j])) {
+                        rolesandtext[j].append(currole + ")" + line + "\n");
+                        currole++;
+                    }
                 }
             }
         }
-        for (int i = 0; i < rolesandtext.size(); i++) {
-            System.out.println(rolesandtext.get(i) + "\n");
+        catch (Exception e){
+            System.out.println(e.getMessage());
         }
 
 
 
+        StringBuilder finaltext = new StringBuilder();
+        for (StringBuilder sb : rolesandtext)
+            finaltext.append(sb + "\n");
+
+        return finaltext.toString();
     }
+
 }
